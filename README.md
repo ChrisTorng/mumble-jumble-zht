d5ac69437cdf50101e0457d9dc8faa3ed3c055e5
ChrisTorng <ChrisTorng@hotmail.com>
2025-06-25 22:44:52 +0800

    新增 @vitejs/plugin-vue 套件以支援本機開發，更新相關配置

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Mumble Jumble 中文版
+
+由 [Google AI Studio - Build](https://aistudio.google.com/apps) 之 [Mumble Jumble](https://aistudio.google.com/apps/bundled/mumble_jumble) 修改為繁體中文版。
+
+增加參考 `@vitejs/plugin-vue` npm 套件以支援本機開發。
+
 # Run and deploy your AI Studio app
 
 This contains everything you need to run your app locally.
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [vue()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
       },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
+          'vue': 'vue/dist/vue.esm-bundler.js'
         }
       }
     };
